3b69ed5f09aa2c87c29cfae5c50687592784ef2f
ShraddhaAgni3 <155717879+ShraddhaAgni3@users.noreply.github.com>
2026-05-01 04:00:34 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   apiUrl:
     window.location.hostname === 'localhost'
       ? 'https://taskmanager-ojep.onrender.com/api'
-      : 'https://task-management-app-8t3d.vercel.app/api',
-  trackingApiUrl: 'https://visitor-tracking-api.vercel.app/api/visit',
+      : 'https://taskmanager-ojep.onrender.com/api',
+  trackingApiUrl: 'https://taskmanager-ojep.onrender.com/api/visit',
 };
